cf735cb553cbf58d334f1e4ddde67e1a20271ae3
prakharbhatt93 <prakharbhatt93@gmail.com>
2025-02-26 13:37:55 +0100

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -13,8 +13,8 @@ int memcheckFailDemo(int* arrayStack, unsigned int arrayStackLen,
   int* array = new int[5];
   array[100] = 0; 
 
-  arrayStack[5] = 0; 
-  arrayHeap[5] = 0; 
+  arrayStack[0] = 0; 
+  arrayHeap[0] = 0; 
 
   for (unsigned int i = 0; i <= arrayStackLen; i++) {
       arrayStack[i] = 0;
